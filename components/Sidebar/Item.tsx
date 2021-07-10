4c6b459ff1eb36ca5655f1c71f7ca0ede959d3ae
jifakir <jifakir@gmail.com>
2021-07-10 16:42:21 +0600

    Fixed mobile version navbar issue

--- components/Sidebar/Item.tsx
+++ components/Sidebar/Item.tsx
@@ -1,4 +1,5 @@
 import * as Ionic from 'react-icons/io5';
+import { useMediaQuery } from 'react-responsive';
 import Link from 'next/link';
 import { useContext } from 'react';
 import { CollapseContext } from '../../context/CollapseContext';
@@ -12,13 +13,15 @@ type ItemProps = {
 
 const Item = ({icon, name, badge, url}:ItemProps) => {
 
+    const isTablet = useMediaQuery({minWidth: 768, maxWidth: 1023}, undefined, (match) => match ? setCollapse(true) : setCollapse(false));
+
     const Icon = Ionic[icon];
 
     const [collapse, setCollapse] = useContext<any>(CollapseContext);
 
     return (
         <Link  href={url} passHref>
-            <div onClick={() => setCollapse(true)} className="relative w-full font-medium cursor-pointer transition-all duration-400 ease-in-out flex justify-between items-center rounded-xl px-5 py-3 text-unity-gray hover:bg-unity-purple hover:text-white">
+            <div onClick={() => isTablet ? setCollapse(true) : ''} className="relative w-full font-medium cursor-pointer transition-all duration-400 ease-in-out flex justify-between items-center rounded-xl px-5 py-3 text-unity-gray hover:bg-unity-purple hover:text-white">
                 <div className={`flex ${collapse ? 'justify-between' : 'justify-between'}`}>
                     <i className="text-lg flex items-center"><Icon /></i>
                     {
@@ -27,7 +30,7 @@ const Item = ({icon, name, badge, url}:ItemProps) => {
                 </div>
                 {
                     badge! >= 0 && (<>
-                            <div className={`rounded-full flex justify-center items-center bg-unity-orange ${!collapse ? 'absolute top-1 right-2 w-4 h-4' : 'w-5 h-5'}`}>
+                            <div className={`rounded-full flex justify-center items-center bg-unity-orange ${collapse ? 'absolute top-1 right-2 w-4 h-4' : 'w-5 h-5'}`}>
                                 <span className="p-2 text-xs text-white">{badge}</span>
                             </div>
                         </>)
